53592f93a790b375c1cca7134a4d904b836c0042
KostadinPlachkov <kostt23@gmail.com>
2017-08-31 19:26:49 +0300

    Initial commit

---
+++ DBAppsIntroduction/DBAppsIntroduction/Startup.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Data.SqlClient;
+
+namespace DBAppsIntroduction
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection(@"
+Server=.;
+Database=DatabaseExample;
+Integrated Security=true");
+            using (connection)
+            {
+                
+            }
+        }
+    }
+}
