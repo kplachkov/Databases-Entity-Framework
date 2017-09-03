4519f489fe665965dbd4e3bf669a0abc9cc91ded
KostadinPlachkov <kostt23@gmail.com>
2017-09-04 00:35:11 +0300

    Added: SqlCommand().Parameters

--- DBAppsIntroduction/DBAppsIntroduction/Startup.cs
+++ DBAppsIntroduction/DBAppsIntroduction/Startup.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Data.Entity.Core.Metadata.Edm;
 using System.Data.SqlClient;
 
 namespace DBAppsIntroduction
@@ -20,18 +21,42 @@ Integrated Security=true");
             connection.Open();
             using (connection)
             {
-                string query = "SELECT COUNT(*) FROM Employees";
-                SqlCommand cmd = new SqlCommand(query, connection);
-                var employeesCount = (int) cmd.ExecuteScalar();
-                Console.WriteLine("Number of employees: {0}", employeesCount);
 
-                query = @"SELECT FirstName,
+                NumberOfEmployees(connection);
+
+                EmployeesFirstNameLastNameSalary(connection);
+
+                InsertTown(connection);
+
+                DeleteTown(connection);
+
+                SearchProjectIdByName(connection);
+
+            }
+
+        }
+
+        static void NumberOfEmployees(SqlConnection connection)
+        {
+            
+            string query = "SELECT COUNT(*) FROM Employees";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            var employeesCount = (int) cmd.ExecuteScalar();
+            Console.WriteLine("Number of employees: {0}", employeesCount);
+            
+        }
+
+        static void EmployeesFirstNameLastNameSalary(SqlConnection connection)
+        {
+            // Read
+            string query = @"SELECT FirstName,
 LastName,
 Salary FROM Employees";
-                cmd = new SqlCommand(query, connection);
-                var reader = cmd.ExecuteReader();
-                using (reader)
-                {
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            var reader = cmd.ExecuteReader();
+            using (reader)
+            {
 //                    reader.Read();  // Fetch a row from the SQL table.
 //                    Console.WriteLine(reader[0]);  // The first entry of the row.
 //                    Console.WriteLine("Names:");
@@ -39,38 +64,67 @@ Salary FROM Employees";
 //                    {
 //                        Console.WriteLine($"- {reader[1]} {reader[2]}");
 //                    }
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    Console.Write($"{reader.GetName(i),-20}");
+                }
+                Console.WriteLine();
+                while (reader.Read())
+                {
                     for (int i = 0; i < reader.FieldCount; i++)
                     {
-                        Console.Write($"{reader.GetName(i), -20}");
+                        Console.Write($"{reader[i],-20}");
                     }
                     Console.WriteLine();
-                    while (reader.Read())
-                    {
-                        for (int i = 0; i < reader.FieldCount; i++)
-                        {
-                            Console.Write($"{reader[i], -20}");
-                        }
-                        Console.WriteLine();
-                    }
-
                 }
 
-                // Insert
-                Console.Write("Enter town name to add: ");
-                string townName = Console.ReadLine();
-                query = $"INSERT INTO Towns VALUES('{townName}')";
-                cmd = new SqlCommand(query, connection);
-                int affectedRows = cmd.ExecuteNonQuery();
-                Console.WriteLine($"Affected rows: {affectedRows}");
-
-                // Delete
-                Console.Write("Enter town name to delete: ");
-                townName = Console.ReadLine();
-                query = $"DELETE FROM Towns WHERE Name = '{townName}'";
-                cmd = new SqlCommand(query, connection);
-                affectedRows = cmd.ExecuteNonQuery();
-                Console.WriteLine($"Affected rows: {affectedRows}");
             }
+        }
+
+        static void InsertTown(SqlConnection connection)
+        {
+            // Insert
+            Console.Write("Enter town name to add: ");
+            string townName = Console.ReadLine();
+            
+            string query = $"INSERT INTO Towns VALUES(@TownName)";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@TownName", townName);  // Adds the variable to the query.
+            int affectedRows = cmd.ExecuteNonQuery();
+            Console.WriteLine($"Affected rows: {affectedRows}");
+            
+        }
+
+        static void DeleteTown(SqlConnection connection)
+        {
+            // Delete
+            Console.Write("Enter town name to delete: ");
+            string townName = Console.ReadLine();
+
+            string query = $"DELETE FROM Towns WHERE Name = @TownName";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@TownName", townName);  // Adds the variable to the query.
+            int affectedRows = cmd.ExecuteNonQuery();
+            Console.WriteLine($"Affected rows: {affectedRows}");
+        }
+
+        static void SearchProjectIdByName(SqlConnection connection)
+        {
+            Console.Write("Enter project name: ");
+            var pattern = Console.ReadLine();
+            string query = @"
+SELECT TOP 1 ProjectId
+FROM Projects
+WHERE Name LIKE @ProjectName";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@ProjectName", "%" + pattern + "%");
+            int foundProjectId = (int) (cmd.ExecuteScalar() ?? -1);
+            if (foundProjectId == -1)
+            {
+                Console.WriteLine("Project not found");
+                return;
+            }
+            Console.WriteLine($"Found project with ID: {foundProjectId}");
         }
     }
 }
